da5f9a26e714a53dbb89534fb73d1d1dcc8cec3e
Trần Lê Ngọc Huy <ngochuydev@gmail.com>
2016-01-27 03:48:13 +0000

    create chef and recipes associations and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126183230) do
+ActiveRecord::Schema.define(version: 20160127032504) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160126183230) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "chef_id"
   end
 
   create_table "users", force: :cascade do |t|
